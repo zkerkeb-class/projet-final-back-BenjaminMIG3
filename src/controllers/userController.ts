8b533421936b8f9b4d28e89ded3c35462fb20990
BenjaminMIG3 <benjamin.dusunceli@gmail.com>
2025-06-26 13:16:06 +0200

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -259,6 +259,15 @@ class UserController {
       });
     }
   }
+
+  async getUserById(req: Request, res: Response) {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ user });
+  } 
 }
 
 export default new UserController();
